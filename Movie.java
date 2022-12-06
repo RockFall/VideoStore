949668aaecbc5fdca74212351f527a4e2bd0d51d
Caio Rocha <72170805+RockFall@users.noreply.github.com>
2022-12-06 00:39:59 +0000

    Commit 12

--- Movie.java
+++ Movie.java
@@ -37,28 +37,11 @@ public class Movie {
   }
 
   public double getCharge(int daysRented){
-    //determine amounts for each line
-    double thisAmount = 0;
-    switch (this.getPriceCode()) {
-        case Movie.REGULAR:
-            thisAmount += 2;
-            if (daysRented > 2)
-                thisAmount += (daysRented - 2) * 1.5;
-            break;
-        case Movie.NEW_RELEASE:
-            thisAmount += daysRented * 3;
-            break;
-        case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (daysRented > 3)
-                thisAmount += (daysRented - 3) * 1.5;
-            break;
-    }
-    return thisAmount;
+    return _price.getCharge(daysRented);
   }
   public int getFrequentRenterPoints(int daysRented){
     if ((this.getPriceCode() == Movie.NEW_RELEASE) &&
     daysRented > 1) return 2;
     return 1;
-  }
+}
 }
